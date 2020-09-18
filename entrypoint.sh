c65a86e85c273cca696594ec4060ac9af03ab624
petems <p.morsou@gmail.com>
2020-09-18 12:39:02 +0100

    Adding more outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,19 @@
 #!/bin/sh -l
-minimumWidth=$1
-minimumHeight=$2
+limitWidth=$1
+limitHeight=$2
 
+echo "Width Limit: $1"
+echo "Height Limit: $2"
+
+echo "Changing to given directory: $3"
 cd $3
-for f in $files
+for f in $FILES
 do
+  echo "Image Name: $f"
   imageWidth=$(identify -format "%w" "$f")
   imageHeight=$(identify -format "%h" "$f")
 
-  if [ "$imageWidth" -gt "$minimumWidth" ] || [ "$imageHeight" -gt "$minimumHeight" ]; then
-      mogrify -resize $4 $f
+  if [ "$imageWidth" -gt "$limitWidth" ] || [ "$imageHeight" -gt "$limitHeight" ]; then
+    mogrify -resize $4 $f
   fi
 done
